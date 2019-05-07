b0afb0b89374378e8f30211b21faac0ede8087e7
Jorge Gómez <jorge.pove77@gmail.com>
2019-05-07 21:42:55 +0200

    Clase 2 spawner

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public Transform spawnPosition;
+    public GameObject crazySphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootSphere();
+    }
+
+    void ShootSphere()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(crazySphere, spawnPosition.position, spawnPosition.rotation);
+        }
+    }
+}
